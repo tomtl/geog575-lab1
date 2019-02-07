06a08ea315bfbd4ae5f8be978334aa19471a4d1f
tlee <tlee@altvil.com>
2019-02-07 16:17:22 -0500

    Complete leaflet geojson tutorial

---
+++ js/leaflet-tutorial-1.js
@@ -0,0 +1,42 @@
+/* Example from Leaflet Quick Start Guide*/
+
+let map = L.map('mapid').setView([51.505, -0.09], 13);
+
+//add tile layer...replace project id and accessToken with your own
+L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap contributors</a>'
+}).addTo(map);
+
+let marker = L.marker([51.5, -0.09]).addTo(map);
+
+let circle = L.circle([51.508, -0.11], 500, {
+    color: 'red',
+    fillColor: '#f03',
+    fillOpacity: 0.5
+}).addTo(map);
+
+let polygon = L.polygon([
+    [51.509, -0.08],
+    [51.503, -0.06],
+    [51.51, -0.047]
+]).addTo(map);
+
+marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
+circle.bindPopup("I am a circle.");
+polygon.bindPopup("I am a polygon.");
+
+let popup = L.popup()
+    .setLatLng([51.5, -0.09])
+    .setContent("I am a standalone popup.")
+    .openOn(map);
+
+popup = L.popup();
+
+function onMapClick(e) {
+    popup
+        .setLatLng(e.latlng)
+        .setContent("You clicked the map at " + e.latlng.toString())
+        .openOn(map);
+}
+
+map.on('click', onMapClick);
--- js/main.js
+++ js/main.js
@@ -1,42 +1,115 @@
 /* Example from Leaflet Quick Start Guide*/
 
-let map = L.map('mapid').setView([51.505, -0.09], 13);
+let map = L.map('mapid').setView([39.756, -104.994], 13);
 
 //add tile layer...replace project id and accessToken with your own
 L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap contributors</a>'
 }).addTo(map);
 
-let marker = L.marker([51.5, -0.09]).addTo(map);
-
-let circle = L.circle([51.508, -0.11], 500, {
-    color: 'red',
-    fillColor: '#f03',
-    fillOpacity: 0.5
-}).addTo(map);
-
-let polygon = L.polygon([
-    [51.509, -0.08],
-    [51.503, -0.06],
-    [51.51, -0.047]
-]).addTo(map);
-
-marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
-circle.bindPopup("I am a circle.");
-polygon.bindPopup("I am a polygon.");
-
-let popup = L.popup()
-    .setLatLng([51.5, -0.09])
-    .setContent("I am a standalone popup.")
-    .openOn(map);
-
-popup = L.popup();
-
-function onMapClick(e) {
-    popup
-        .setLatLng(e.latlng)
-        .setContent("You clicked the map at " + e.latlng.toString())
-        .openOn(map);
+function onEachFeature(feature, layer) {
+  if (feature.properties && feature.properties.popupContent) {
+    layer.bindPopup(feature.properties.popupContent);
+  }
 }
 
-map.on('click', onMapClick);
+// points
+let geojsonFeature = [{
+    "type": "Feature",
+    "properties": {
+      "name": "Coors Field",
+      "amenity": "Baseball stadium",
+      "popupContent": "This is where the Rockies play!",
+      "show_on_map": true
+    },
+    "geometry": {
+      "type": "Point",
+      "coordinates": [-104.99404, 39.75621]
+    }
+  }, {
+    "type": "Feature",
+    "properties": {
+        "name": "Busch Field",
+        "show_on_map": false
+    },
+    "geometry": {
+        "type": "Point",
+        "coordinates": [-104.98404, 39.74621]
+    }
+}];
+
+let geojsonMarkerOptions = {
+  radius: 8,
+  fillColor: "#ff7800",
+  color: "#000",
+  weight: 1,
+  opacity: 1,
+  fillOpacity: 0.8
+};
+
+L.geoJSON(geojsonFeature, {
+  pointToLayer: function(feature, latlng) {
+    return L.circleMarker(latlng, geojsonMarkerOptions);
+  },
+  filter: function(feature, layer) {
+    return feature.properties.show_on_map;
+  },
+  onEachFeature: onEachFeature
+}).addTo(map);
+
+// lines
+let myLines = [{
+  "type": "LineString",
+  "coordinates": [[-100, 40], [-105, 45], [-110, 55]]
+}, {
+  "type": "LineString",
+  "coordinates": [[-105, 40], [-110, 45], [-115, 55]]
+}];
+
+let myStyle = {
+    "color": "#ff7800",
+    "weight": 5,
+    "opacity": 0.65
+};
+
+L.geoJSON(myLines, {
+    style: myStyle
+}).addTo(map);
+
+// polygons
+let states = [{
+    "type": "Feature",
+    "properties": {"party": "Republican"},
+    "geometry": {
+        "type": "Polygon",
+        "coordinates": [[
+            [-104.05, 48.99],
+            [-97.22,  48.98],
+            [-96.58,  45.94],
+            [-104.03, 45.94],
+            [-104.05, 48.99]
+        ]]
+    }
+}, {
+    "type": "Feature",
+    "properties": {"party": "Democrat"},
+    "geometry": {
+        "type": "Polygon",
+        "coordinates": [[
+            [-109.05, 41.00],
+            [-102.06, 40.99],
+            [-102.03, 36.99],
+            [-109.04, 36.99],
+            [-109.05, 41.00]
+        ]]
+    }
+}];
+
+L.geoJSON(states, {
+    style: function(feature) {
+        switch (feature.properties.party) {
+            case 'Republican': return {color: "#ff0000"};
+            case 'Democrat':   return {color: "#0000ff"};
+        }
+    }
+}).addTo(map);
