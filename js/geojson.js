625114fb93133b178d1d2a53e6c3c98bde254102
Tom Lee <tomtleee@gmail.com>
2019-02-11 08:25:16 -0500

    Add point to layer

--- js/geojson.js
+++ js/geojson.js
@@ -17,8 +17,23 @@ function getData(map){
     $.ajax("data/megacities.geojson", {
         dataType: "json",
         success: function(response){
-            //create a Leaflet GeoJSON layer and add it to the map
-            L.geoJson(response).addTo(map);
-        }
-}); };
+          //create marker options
+          var geojsonMarkerOptions = {
+              radius: 8,
+              fillColor: "#ff7800",
+              color: "#000",
+              weight: 1,
+              opacity: 1,
+              fillOpacity: 0.8
+          };
+
+          //create a Leaflet GeoJSON layer and add it to the map
+        L.geoJson(response, {
+            pointToLayer: function (feature, latlng){
+                return L.circleMarker(latlng, geojsonMarkerOptions);
+            }
+        }).addTo(map);
+      }
+  });
+};
 $(document).ready(createMap);
