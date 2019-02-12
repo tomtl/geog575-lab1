8905466a92256f7b7148d7c56af33d547bb3ccbe
tlee <tlee@altvil.com>
2019-02-12 17:01:30 -0500

    Add proportional symbols

--- js/geojson.js
+++ js/geojson.js
@@ -25,6 +25,50 @@ function onEachFeature(feature, layer) {
     };
 };
 
+//calculate the radius of each proportional symbol
+function calcPropRadius(attValue) {
+    //scale factor to adjust symbol size evenly
+    var scaleFactor = (1.0 / 1000000);
+    //area based on attribute value and scale factor
+    var area = attValue * scaleFactor;
+    //radius calculated based on area
+    var radius = Math.sqrt(area/Math.PI);
+
+    return radius;
+};
+
+// proportion circle markers
+function createPropSymbols(data, map){
+    let attribute = "Total";
+    let year = 2017;
+
+    //create marker options
+    var geojsonMarkerOptions = {
+        radius: 8,
+        fillColor: "#ff7800",
+        color: "#000",
+        weight: 1,
+        opacity: 1,
+        fillOpacity: 0.8
+    };
+
+    //create a Leaflet GeoJSON layer and add it to the map
+    L.geoJson(data, {
+        filter: function(feature, layer) {
+            if (feature.properties.Year == year) {
+              return true;
+            }
+        },
+        pointToLayer: function (feature, latlng) {
+            var attValue = Number(feature.properties[attribute]);
+            // console.log(feature.properties);
+            geojsonMarkerOptions.radius = calcPropRadius(attValue);
+            return L.circleMarker(latlng, geojsonMarkerOptions);
+        },
+        onEachFeature: onEachFeature
+    }).addTo(map);
+};
+
 //function to retrieve the data and place it on the map
 function getData(map){
     //load the data
@@ -32,26 +76,7 @@ function getData(map){
         dataType: "json",
         success: function(response){
 
-          //create marker options
-          var geojsonMarkerOptions = {
-              radius: 8,
-              fillColor: "#ff7800",
-              color: "#000",
-              weight: 1,
-              opacity: 1,
-              fillOpacity: 0.8
-          };
-
-        //create a Leaflet GeoJSON layer and add it to the map
-        L.geoJson(response, {
-            filter: function(feature, layer) {
-              return feature.properties.Year = 2017;
-            },
-            pointToLayer: function (feature, latlng){
-                return L.circleMarker(latlng, geojsonMarkerOptions);
-            },
-            onEachFeature: onEachFeature
-        }).addTo(map);
+        createPropSymbols(response, map);
       }
   });
 };
